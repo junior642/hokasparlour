040eed2aff7fd2c5088e06ca42aa3e6aa82c1203
junior642 <omwengav99@gmail.com>
2026-03-18 21:46:35 +0000

    update - remove backup file

--- parlour/views.py
+++ parlour/views.py
@@ -699,7 +699,7 @@ def user_login(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 next_url = request.GET.get('next', 'home')
